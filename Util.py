0b0538bf4116eef8e6df76681b13df456dba2df6
Phu Tran <tr.ngocphu@gmail.com>
2019-08-29 13:59:35 +0800

    update Util.py

--- Util.py
+++ Util.py
@@ -10,13 +10,7 @@
 #
 
 import math
-
-
-const double TauMin = 0.0001; // A minimum time for positive tau
-
-const double NaR = 9999999;   // An arbitrary large number
-
-const double Pi = 3.141592654; // Value of Pi 
+from Constants import *
 
 # square
 def sq( x ) :
@@ -57,4 +51,98 @@ def discr( a, b, c ) :
 # Quadratic equation
 def root( a, b, c, eps ) :
     if a == 0 :
-        return None 
+        return NAR # this case should never happen
+    d = discr( a, b, c )
+    if ( d < 0 ) :
+        return NAR # this case should never happen
+    return ( -b + eps * math.sqrt( d ) / (2*a) )
+
+
+# To range ( -pi, pi ]
+def topi( x ) :
+    while x <= -PI or PI < x :
+        if ( x <= -PI ) :
+            x += 2*PI
+        else :
+            x -= 2*PI
+    return x
+
+
+# To range [0, 360)
+def to360( x ) :
+    while x < 0 or 360 <= x :
+        if x < 0 :
+            x += 360
+        else :
+            x -= 360
+    return x
+
+
+# Radians to degrees in [0, 360)
+def rad2deg( x ) :
+    return to360( x * 180 / PI )
+
+
+# Degrees to radians in (-pi, pi]
+def deg2rad( x ) :
+    return topi( x * PI / 180.0 )
+
+
+# Degrees:Minutes to radians
+def degmin2rad( d, m ) :
+    return ( d + sign(d) * m / 60.0 ) * PI / 180.0
+
+
+# Meters to nautical miles
+def m2nm( x ) :
+    return x / 1852
+
+
+# Nautical miles to meters 
+def nm2m( x ) :
+    return x * 1852
+
+
+# Knots to meters/seconds
+def knots2msec( x ) :
+    return nm2m(x) / 3600.0
+
+
+# Meters/seconds to knots
+def msec2knots( x ) :
+    return m2nm(x) * 3600
+
+
+# Feet to meters
+def ft2m( x ) :
+    return 1.609 * x / 5.28
+
+
+# Meters to feet 
+def m2ft( x ) :
+    return 5.28 * x / 1.609
+
+
+# Feets/minutes to meters/seconds
+def ftmin2msec( x ) :
+    return ft2m(x) / 60.0
+
+
+# Meters/seconds to feets/minutes
+def msec2ftmin( x ) :
+    return m2ft(x) * 60 
+
+
+# Sign ( 0 is positive )
+def sign( x ) :
+    return 1 if x >= 0 else -1
+
+
+# ground speed to vx (trk [rad] in true North-clockwise convention) 
+def gs2vx( gs, trk ) :
+    return gs * sin(trk)
+
+
+# ground speed to vy (trk [rad] in true North-clockwise convention)
+def gs2vy( gs, trk ) :
+    return gs * cos(trk)
