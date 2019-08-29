be3f5f006116c46d15fd8f90b16bbc5531761cc9
Phu Tran <tr.ngocphu@gmail.com>
2019-08-29 16:10:07 +0800

    update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,5 +1,8 @@
-TAU_MIN = 0.0001; # A minimum time for positive tau
+# Constants used in Util.py
+TAU_MIN = 0.0001 # A minimum time for positive tau
+NAR = 9999999   # An arbitrary large number
+PI = 3.141592654 # Value of Pi 
 
-NAR = 9999999;   # An arbitrary large number
-
-PI = 3.141592654; # Value of Pi 
+# Constants used in Geodesic.py
+WGS84 = 6378137 # Major axis [m]
+Flat  = 1 / 298.257223563 # Earth flattening
