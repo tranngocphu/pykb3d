e838038114703fe663196ca1ad84cc7c64f8cc10
Phu Tran <tr.ngocphu@gmail.com>
2019-08-29 19:43:43 +0800

    update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,6 +1,6 @@
 # Constants used in Util.py
 TAU_MIN = 0.0001 # A minimum time for positive tau
-NAR = 9999999   # An arbitrary large number
+NaR = 9999999   # An arbitrary large number
 PI = 3.141592654 # Value of Pi 
 
 # Constants used in Geodesic.py
